de183cdc5e8d298d6d94dad340e8e215e1531a82
gui-rossi <guizo.rossi@gmail.com>
2023-11-09 22:40:05 -0300

    supporting base64

--- src/components/EventsPage.vue
+++ src/components/EventsPage.vue
@@ -5,21 +5,22 @@
       <button @click="requestInfo">Request Infos</button>
 
       <div v-if="this.image">
-          <img src="../assets/00-06-54.jpg" alt="Requested Image" />
+        <img :src="image" alt="Requested Image" />
       </div>
-  
+
       <div v-if="this.infos?.gps">
-          <p>GPS Coordinates: {{ this.infos.gps }}</p>
+        <p>GPS Coordinates: {{ this.infos.gps }}</p>
       </div>
-  
+
       <div v-if="this.infos?.battery">
-          <p>Battery: {{ this.infos.battery }}</p>
-          <p>Gps: {{ this.infos.gps }}</p>
+        <p>Battery: {{ this.infos.battery }}</p>
+        <p>Gps: {{ this.infos.gps }}</p>
       </div>
     </div>
     <ul v-if="eventLogHistory?.length">
       <li v-for="eventLog in eventLogHistory" :key="eventLog.Id">
-        {{ "LogType: " + eventLog.LogType + "Status: " + eventLog.Status + "Value: " + eventLog.Value + "Date: " + eventLog.TimeStamp }}
+        {{ "LogType: " + eventLog.LogType + "Status: " + eventLog.Status + "Value: " + eventLog.Value + "Date: " +
+          eventLog.TimeStamp }}
       </li>
     </ul>
   </div>
@@ -27,57 +28,64 @@
   
 <script>
 /* eslint-disable */
-  import axios from '../axios';
+import axios from '../axios';
 
-  export default {
-    props: {
-        infos: {
-            type: Array,
-            required: false,
-        },
-        image: {
-            type: String,
-            required: false,
-        },
+export default {
+  props: {
+    infos: {
+      type: Array,
+      required: false,
     },
-    mounted: async function () {
-      await this.loadData();
+  },
+  mounted: async function () {
+    await this.loadData();
+  },
+  data() {
+    return {
+      eventLogHistory: null,
+      image: null,
+    };
+  },
+  methods: {
+    async loadData() {
+      try {
+        const data = await axios.get('/Event/GetHistory');
+        this.eventLogHistory = data;
+      } catch (error) {
+        console.error(error);
+      }
     },
-    data() {
-        return {
-          eventLogHistory: null,
-        };
+    async requestImage() {
+      try {
+        await axios.get('/Event/RequestImage');
+      } catch (error) {
+        console.error(error);
+      }
     },
-    methods: {
-      async loadData() {
-        try {
-          const data = await axios.get('/Event/GetHistory');
-          this.eventLogHistory = data;
-        } catch (error) {
-          console.error(error);
-        }
-      },
-      async requestImage() {
-        try {
-          await axios.get('/Event/RequestImage');
-        } catch (error) {
-          console.error(error);
-        }
-      },
-      async requestInfo() {
-        try {
-          await axios.get('/Event/RequestInfos');
-        } catch (error) {
-          console.error(error);
-        }
-      },
+    async requestInfo() {
+      try {
+        await axios.get('/Event/RequestInfos');
+      } catch (error) {
+        console.error(error);
+      }
     },
-  };
+    getBase64(file) {
+      var reader = new FileReader();
+      reader.readAsDataURL(file);
+      reader.onload = function () {
+        return reader.result;
+      };
+      reader.onerror = function (error) {
+        console.log('Error: ', error);
+      };
+    }
+  },
+};
 </script>
 
 <style>
 img {
-    max-width:100%;
+  max-width: 100%;
 }
 
 .row {
